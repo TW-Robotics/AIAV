f61ee6f80742a877e866a69d43a155f1d3409d87
Alessandro Scherl <alessandro.scherl@technikum-wien.at>
2023-06-09 15:44:15 +0200

    update buildandrun.sh

--- AI_Modelle_lernen_nicht_das_was_sie_lernen_sollten/buildandrun.sh
+++ AI_Modelle_lernen_nicht_das_was_sie_lernen_sollten/buildandrun.sh
@@ -11,4 +11,7 @@
 docker build . -t lrp
 
 # Ausführen des Containers 
-docker run -it --rm -t -d --name LRP -e DISPLAY=$DISPLAY -p 8888:8888 --mount src="$(pwd)",target=/workspace/,type=bind lrp jupyter-lab --ip 0.0.0.0 --IdentitiyProvider.token='aiav' --no-browser --allow-root
+docker run -it --rm -t -d --name LRP -e DISPLAY=$DISPLAY -p 8888:8888 --mount src="$(pwd)",target=/workspace/,type=bind lrp 
+
+docker exec LRP -c "jupyter-lab --ip 0.0.0.0 --IdentitiyProvider.token='aiav' --no-browser --allow-root"
+
