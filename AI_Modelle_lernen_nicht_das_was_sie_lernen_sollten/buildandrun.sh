580d59767e94e87b02aa4b7ea350095c0616b48c
Alessandro Scherl <alessandro.scherl@technikum-wien.at>
2023-06-09 15:54:38 +0200

    fixed typos

--- AI_Modelle_lernen_nicht_das_was_sie_lernen_sollten/buildandrun.sh
+++ AI_Modelle_lernen_nicht_das_was_sie_lernen_sollten/buildandrun.sh
@@ -14,4 +14,3 @@ docker build . -t lrp
 docker run -it --rm -t -d --name LRP -e DISPLAY=$DISPLAY -p 8888:8888 --mount src="$(pwd)",target=/workspace/,type=bind lrp 
 
 docker exec LRP /bin/sh -c "jupyter-lab --ip 0.0.0.0 --IdentitiyProvider.token='aiav' --no-browser --allow-root"
-
